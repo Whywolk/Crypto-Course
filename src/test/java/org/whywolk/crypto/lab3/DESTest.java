c1114820757da198dfbe81da08dd3d6945f240c3
Whywolk <alexshir2001@gmail.com>
2022-10-03 23:48:21 +0300

    Started to implement DES algorithm

---
+++ src/test/java/org/whywolk/crypto/lab3/DESTest.java
@@ -0,0 +1,27 @@
+package org.whywolk.crypto.lab3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DESTest extends Assert {
+
+    @Test
+    public void testInitialPermutation() {
+        byte[] block = new byte[] {
+                (byte) 0b01010101, (byte) 0b01010101, (byte) 0b01010101, (byte) 0b01010101,
+                (byte) 0b01010101, (byte) 0b01010101, (byte) 0b01010101, (byte) 0b01010101
+        };
+        byte[] expected = new byte[] {
+                (byte) 0b11111111, (byte) 0b11111111, (byte) 0b11111111, (byte) 0b11111111,
+                (byte) 0b00000000, (byte) 0b00000000, (byte) 0b00000000, (byte) 0b00000000
+        };
+
+        byte[] IP = DES.initialPermutation(block);
+
+        assertArrayEquals(expected, IP);
+    }
+//
+//    @Test
+//    public void
+}
